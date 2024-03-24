37b0dce1c6384f921d83d70902cf4fa0542253fc
Moheb-Faiz <mohebrahman.faiz@mail.concordia.ca>
2024-03-23 22:38:36 -0400

    new version.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ShowPackagesComponent} from "./components/show-packages/show-packages.component";
+import {AddPackagesComponent} from "./components/add-packages/add-packages.component";
+import {UpdatePackagesComponent} from "./components/update-packages/update-packages.component";
+import {HomeComponent} from "./components/home/home.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // {path:'', component:HomeComponent},
+  {path:'show', component: ShowPackagesComponent},
+  {path:'add', component: AddPackagesComponent},
+  {path:'update/:id', component: UpdatePackagesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddPackagesComponent } from './components/add-packages/add-packages.component';
+import { UpdatePackagesComponent } from './components/update-packages/update-packages.component';
+import { ShowPackagesComponent } from './components/show-packages/show-packages.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPackagesComponent,
+    UpdatePackagesComponent,
+    ShowPackagesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
